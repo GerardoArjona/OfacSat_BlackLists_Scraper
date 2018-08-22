b902d44637f2630d5b3d70e04526008d240a37fb
Gerardo Arjona <gerardo.arjona@konsigue.com>
2018-08-22 18:03:50 -0500

    Add SAT Scraping
    Signed-off-by: Gerardo Arjona <gerardo.arjona@konsigue.com>

--- ofac_scrape.py
+++ ofac_scrape.py
@@ -3,13 +3,26 @@ import urllib.request as req
 import codecs
 import sys
 
+
+#OFAC
 url = "https://www.treasury.gov/ofac/downloads/sdn.csv"
 ftpstream = req.urlopen(url)
-obtained_csv = csv.reader(codecs.iterdecode(ftpstream, 'utf-8'))
+obtained_csv_ofac= csv.reader(codecs.iterdecode(ftpstream, 'utf-8'))
+
+#SAT
+opened_csv=open('sdn.csv', 'r')
+obtained_csv_sat = csv.reader(opened_csv)
 
 def scrape(**kwargs):
-    found=[line for line in obtained_csv if [line for element in line if kwargs['name'].lower() in element.lower() or kwargs['rfc'].lower() in element.lower() ]]   
-    for i in found:
+
+    found_OFAC=[line for line in obtained_csv_ofac if [line for element in line if kwargs['name'].lower() in element.lower() or kwargs['rfc'].lower() in element.lower() ]]
+    found_SAT=[line for line in obtained_csv_sat if [line for element in line if kwargs['name'].lower() in element.lower() or kwargs['rfc'].lower() in element.lower() ]]     
+    print("OFAC: ")
+    for i in found_OFAC:
+        print()
+        print(i)
+    print("SAT ")
+    for i in found_SAT:
         print()
         print(i)
 
